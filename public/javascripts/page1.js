bd171eb0462eaebcd78f765eeec1c9541fe4fc75
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-05-29 10:15:47 +0900

    added opening hours

--- public/javascripts/page1.js
+++ public/javascripts/page1.js
@@ -1,14 +1,25 @@
 
-$(document).ready(function(){
-   var ele= $('.give_number');
-   ele.hide();
-   setTimeout(function(){
-       ele.show();
-   },1500);
-   var user_number = $('.display_number');
-   user_number.hide();
-   setTimeout(function(){
-       user_number.show()
-   },3500)
-  
+$(document).ready(function () {
+    var close = $('.closed')
+    var ele = $('.give_number');
+    var user_number = $('.display_number');
+    ele.hide()
+    close.hide()
+    user_number.hide();
+    var day = moment().format('ddd');
+    var time = moment().format('H');
+    if( (time >= 10 && time <= 15)&& !(time>11 && time<13) && !(day === 'Sat') && !(day ==='Sun')) {
+        console.log('inside if statemet executed')
+        setTimeout(function () {
+            ele.show();
+        }, 1500);
+
+        setTimeout(function () {
+            user_number.show()
+        }, 3500)
+    }
+    else {
+        console.log('inside else statement executed')
+        close.show()
+    }
 })
