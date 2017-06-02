539fea9f91eb25c7e21aefccd9cce2f5a0664d2b
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-06-02 13:20:03 -0400

    mlab added for heroku deployment

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 var config = {}
 config.db = {};
    config.db.host= 'localhost';
+   config.db.mLab= 'arandriamihaja:cTGagabJft'
+   //'mongodb://arandriamihaja:cTGagabJft@ds163181.mlab.com:63181/url_shortener_antsa'
     config.db.name= 'url_shortener';
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'http://localhost:3000/'||heroku_host;
 config.secret_key = 'sk_test_b9Y1InUSB94t9sivG7qan5bL';
 module.exports = { config };
+var heroku_host = 'https://shrouded-stream-93491.herokuapp.com'
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const {config} = require('../../config.js')
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://'+config.db.host+'/'+config.db.name);
+var MONGOLAB_URI =  'mongodb://'+config.db.mLab+'@ds163181.mlab.com:63181/url_shortener_antsa';
+mongoose.connect(process.env.MONGOLAB_URI|| 'mongodb://'+config.db.host+'/'+config.db.name);
 
 module.exports= {mongoose};
