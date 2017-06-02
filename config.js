d235f1812e17a7e9aa63894620fdbc8efb3a1dfb
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-06-02 13:24:24 -0400

    heroku host env added

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.db = {};
    config.db.mLab= 'arandriamihaja:cTGagabJft'
    //'mongodb://arandriamihaja:cTGagabJft@ds163181.mlab.com:63181/url_shortener_antsa'
     config.db.name= 'url_shortener';
-config.webhost = 'http://localhost:3000/'||heroku_host;
+config.webhost = process.env.HOST || 'http://localhost:3000/';
 config.secret_key = 'sk_test_b9Y1InUSB94t9sivG7qan5bL';
 module.exports = { config };
 var heroku_host = 'https://shrouded-stream-93491.herokuapp.com'
